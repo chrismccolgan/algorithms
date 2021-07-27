cce68855b19150580cf8657f5f002d5b58a759de
Chris McColgan <mccolganchrisj@gmail.com>
2021-07-27 17:04:19 -0500

    multiple pointers, sliding window

---
+++ multiplePointers.js
@@ -0,0 +1,52 @@
+/* 
+  Write a function called sumZero which accepts 
+  a sorted array of integers. The function should 
+  find the first pair where the sum is 0. Return an
+  array that includes both values that sum to zero 
+  or undefined if a pair does not exist.
+
+  sumZero([1,2,3]) // undefined
+  sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+*/
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+/*
+  Implement a function called countUniqueValues which
+  accepts a sorted array and counts the unique values
+  in the array. There can be negative numbers in the
+  array, but it will always be sorted.
+
+  countUniqueValues([1,1,1,2]) // 2
+  countUniqueValues([]) // 0
+  countUniqueValues([-2,-1,-1,0,1]) // 4
+*/
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
